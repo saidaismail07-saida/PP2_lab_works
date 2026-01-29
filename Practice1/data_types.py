a56a5751eb8efe1f30b1b4e86fedf0f93bb97fdc
IsmailSaida <saida_ismail07@mail.ru>
2026-01-30 00:27:49 +0500

    Practice1 in progress

---
+++ Practice1/data_types.py
@@ -0,0 +1,19 @@
+#1
+x=10 #int
+y=3.14 #float
+name="Bob" #str
+
+#2
+numbers = [1,2,3] #list
+letters= [ a,b,c] #list
+
+#3
+x=tuple(("apple", "banana")) #tuple
+z=set(("apple","cherry")) #set
+
+#4
+a=bool(6) #bool
+b=bytes(8) #bytes
+
+#5
+m=memoryview((bytes(9))) #memorview
