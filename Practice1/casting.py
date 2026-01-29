310b219ed8ec342216d0ee8c14dad5e5e9443edb
IsmailSaida <saida_ismail07@mail.ru>
2026-01-30 00:47:40 +0500

    Practice1 in progress

---
+++ Practice1/casting.py
@@ -0,0 +1,18 @@
+#1
+x=int(1) #x will be 1
+y=int(2.999) #y will be 2
+
+#2
+a=float(1) #a will be 1
+b=float(2.9) #b will be 2.9
+
+#3
+m=str("s1") #m will be s1
+c=str(4.0) #c will be 4.0
+
+#4
+d=int(4.7) #d will be 4
+e=float(5.9) #e will be 5.9
+
+#5
+s=float("3") #s will be 3
