610f6d43d0c14ff59986295097c0ebd40f4907b9
IsmailSaida <saida_ismail07@mail.ru>
2026-01-30 00:12:34 +0500

    Practice1 in progress

---
+++ Practice1/comments.py
@@ -0,0 +1,29 @@
+#1
+
+#This is comment
+print("Hello, dear student!")
+
+#2
+
+#print("Hello, World!")
+print("Cheers, Saida!")
+
+#3
+
+#This is comment
+x=6 #comment
+#Python ignores this line
+print("Ignore!")
+#End
+
+#4
+
+#print("Say hi!")
+print("She said me to say hi")
+
+#5
+#This is comment
+x=11
+#another comment
+print(x)
+#End
