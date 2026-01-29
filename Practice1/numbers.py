f362b71d24951383e7a53c09c8d6d73fc93eedcc
IsmailSaida <saida_ismail07@mail.ru>
2026-01-30 00:40:12 +0500

    Practice1 in progress

---
+++ Practice1/numbers.py
@@ -0,0 +1,43 @@
+#1
+x=1 #int
+y=2.11 #float
+z=1j #complex
+
+#2
+a=1
+b=19757
+c=896522
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+#3
+m=1.1
+n=1.999
+t=-8.09
+
+print(type(m))
+print(type(n))
+print(type(t))
+
+#4
+x=2
+y=3.1
+z=45j
+
+a=float(x)
+b=int(y)
+c=complex(z)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+#5
+a=2+3 #int
+b=9/3 #int
