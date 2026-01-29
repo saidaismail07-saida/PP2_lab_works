dcacfed63e90bd4f0bd582d4c4af5bfa5c00cac2
IsmailSaida <saida_ismail07@mail.ru>
2026-01-30 00:57:00 +0500

    Practice1 in progress

---
+++ Practice1/strings.py
@@ -0,0 +1,19 @@
+#1
+print("It is ok!")
+
+#2
+a="Goodbye"
+print(a)
+
+#3
+b="Babyboss"
+print(b[2])
+
+#4
+z="Hello, it is KBTU!"
+print(len(z))
+
+#5
+txt="The best things in life are free!"
+if"are" in txt:
+    print("Yes, 'are' present ")
