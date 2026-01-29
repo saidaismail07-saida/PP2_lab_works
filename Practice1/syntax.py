a83167ed9c1be48ff113c2ba831c11976e7d2f92
IsmailSaida <saida_ismail07@mail.ru>
2026-01-29 23:58:55 +0500

    Practice1 in progress

---
+++ Practice1/syntax.py
@@ -0,0 +1,23 @@
+#1
+if 5>2:
+    print("Five is greater than two!")
+
+
+#2
+x=1
+y=2
+print(x+y)
+
+#3
+if x>y:
+    print("x is greater than y!")
+
+#4
+if x==y:
+    print("x is equal to y!")
+
+#5
+x=1
+y=3
+print(x)
+print(y)
