dca21428b5296a4e45cde523e674a3b3951ecd50
Saida Ismail <saida_ismail07@mail.ru>
2026-02-06 23:05:01 +0500

    Practice2 in progress

---
+++ Practice2/if_else.py
@@ -0,0 +1,34 @@
+#1
+x = 5
+if x > 10:
+    print("x > 10")
+else:
+    print("x <= 10")
+
+#2
+x = 8
+if x % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+#3
+y = -3
+if y >= 0:
+    print("Positive")
+else:
+    print("Negative")
+
+#4
+score = 60
+if score >= 50:
+    print("Pass")
+else:
+    print("Fail")
+    
+#5
+age = 15
+if age >= 18:
+    print("Adult")
+else:
+    print("Minor")
