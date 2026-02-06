c0e48917eb0cfa6f8410be58c22c4676953e4c90
Saida Ismail <saida_ismail07@mail.ru>
2026-02-06 23:03:24 +0500

    Practice2 in progress

---
+++ Practice2/if_statement.py
@@ -0,0 +1,20 @@
+#1
+x = 10
+if x > 5:
+    print("x is greater than 5")
+
+#2
+if 2 < x:
+    print("x is greater than 2")
+
+#3
+if x == 10:
+    print("x is 10")
+
+#4
+if x != 0:
+    print("x is not zero")
+    
+#5
+if x < 20:
+    print("x is less than 20")
