ca29787571146685c1654a90514e2ca6500a9f71
Saida Ismail <saida_ismail07@mail.ru>
2026-02-06 23:52:21 +0500

    Practice2 in progress

---
+++ Practice2/for_loops.py
@@ -0,0 +1,19 @@
+#1
+for i in range(5):
+    print(i)
+
+#2
+for x in [1, 2, 3]:
+    print(x)
+
+#3
+for char in "Python":
+    print(char)
+
+#4
+for num in range(2, 7):
+    print(num)
+    
+#5
+for i in range(1, 10, 2):
+    print(i)
