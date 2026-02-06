0e22d085d2a505400b4a9fe11de3a93d8589e708
Saida Ismail <saida_ismail07@mail.ru>
2026-02-06 23:56:17 +0500

    Practice2 in progress

---
+++ Practice2/for_loop_continue.py
@@ -0,0 +1,29 @@
+#1
+for i in range(5):
+    if i == 3:
+        continue
+    print(i)
+
+#2
+for x in [1, 2, 3, 4]:
+    if x % 2 == 0:
+        continue
+    print(x)
+
+#3
+for char in "Python":
+    if char == "o":
+        continue
+    print(char)
+
+#4
+for i in range(1, 6):
+    if i == 2:
+        continue
+    print(i)
+    
+#5
+for x in range(10):
+    if x > 5:
+        continue
+    print(x)
