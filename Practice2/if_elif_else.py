d1d3b09f538cb57773602ceb5b4b2b88740a8e7f
Saida Ismail <saida_ismail07@mail.ru>
2026-02-06 23:06:39 +0500

    Practice2 in progress

---
+++ Practice2/if_elif_else.py
@@ -0,0 +1,44 @@
+#1
+x = 0
+if x > 0:
+    print("Positive")
+elif x == 0:
+    print("Zero")
+else:
+    print("Negative")
+
+#2
+score = 85
+if score >= 90:
+    print("A")
+elif score >= 75:
+    print("B")
+else:
+    print("C")
+
+#3
+num = 10
+if num < 0:
+    print("Negative")
+elif num == 0:
+    print("Zero")
+else:
+    print("Positive")
+
+#4
+age = 20
+if age < 13:
+    print("Child")
+elif age < 20:
+    print("Teen")
+else:
+    print("Adult")
+    
+#5
+temperature = 25
+if temperature < 0:
+    print("Freezing")
+elif temperature < 20:
+    print("Cold")
+else:
+    print("Warm")
