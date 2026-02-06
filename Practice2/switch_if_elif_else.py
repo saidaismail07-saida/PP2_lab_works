9d56cb6c6ad574387b4fd58eaabf5a723495401c
Saida Ismail <saida_ismail07@mail.ru>
2026-02-06 23:13:06 +0500

    Practice2 in progress

---
+++ Practice2/switch_if_elif_else.py
@@ -0,0 +1,48 @@
+#1
+day = 3
+if day == 1:
+    print("Monday")
+elif day == 2:
+    print("Tuesday")
+elif day == 3:
+    print("Wednesday")
+else:
+    print("Other day")
+
+#2
+grade = "B"
+if grade == "A":
+    print("Excellent")
+elif grade == "B":
+    print("Good")
+elif grade == "C":
+    print("Average")
+else:
+    print("Poor")
+
+#3
+color = "red"
+if color == "red":
+    print("Stop")
+elif color == "green":
+    print("Go")
+else:
+    print("Slow")
+
+#4
+num = 2
+if num == 1:
+    print("One")
+elif num == 2:
+    print("Two")
+else:
+    print("Other")
+    
+#5
+fruit = "apple"
+if fruit == "apple":
+    print("Fruit is apple")
+elif fruit == "banana":
+    print("Fruit is banana")
+else:
+    print("Other fruit")
