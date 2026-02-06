520273c27ed1413c41671052a0f4285e8005cb7d
Saida Ismail <saida_ismail07@mail.ru>
2026-02-06 23:53:38 +0500

    Practice2 in progress

---
+++ Practice2/for_loop_break.py
@@ -0,0 +1,29 @@
+#1
+for i in range(5):
+    if i == 3:
+        break
+    print(i)
+
+#2
+for x in [1, 2, 3, 4]:
+    if x > 2:
+        break
+    print(x)
+
+#3
+for char in "Python":
+    if char == "h":
+        break
+    print(char)
+
+#4
+for i in range(10):
+    if i == 7:
+        break
+    print(i)
+    
+#5
+for x in range(1, 6):
+    if x == 4:
+        break
+    print(x)
