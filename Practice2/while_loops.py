ef890fd0061c6f1fac6e7f38cc801a98d776df64
Saida Ismail <saida_ismail07@mail.ru>
2026-02-06 23:46:32 +0500

    Practice2 in progress

---
+++ Practice2/while_loops.py
@@ -0,0 +1,29 @@
+#1
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
+
+#2
+n = 0
+while n < 3:
+    print("Hello")
+    n += 1
+
+#3
+x = 10
+while x > 0:
+    print(x)
+    x -= 2
+
+#4
+count = 1
+while count <= 4:
+    print("Count:", count)
+    count += 1
+    
+#5
+y = 3
+while y != 0:
+    print(y)
+    y -= 1
